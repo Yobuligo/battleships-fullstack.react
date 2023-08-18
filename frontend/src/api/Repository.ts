cd87e041cd0b16af518bfa039d124366226312db
Peter <hoffmannpeter82@gmail.com>
2023-08-18 10:27:34 +0200

    Implement isOutdated for frontend

--- frontend/src/api/Repository.ts
+++ frontend/src/api/Repository.ts
@@ -1,8 +1,10 @@
+import { IRepository } from "../shared/api/IRepository";
 import { IEntity } from "../shared/types/IEntity";
 import { IEntityDetails } from "../shared/types/IEntityDetails";
-import { IRepository } from "../shared/types/IRepository";
 
 export class Repository<T extends IEntity> implements IRepository<T> {
+  private _lastVersion = new Date();
+
   constructor(private readonly path: string) {}
 
   add(entity: IEntityDetails<T>): Promise<T> {
@@ -34,6 +36,24 @@ export class Repository<T extends IEntity> implements IRepository<T> {
   findAll(): Promise<T[]> {
     return new Promise(async (resolve) => {
       const response = await fetch(this.url, {
+        mode: "cors",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      resolve(data);
+    });
+  }
+
+  get isOutdated(): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      const version = await this.version;
+      resolve(version !== this._lastVersion);
+    });
+  }
+
+  get version(): Promise<Date> {
+    return new Promise(async (resolve) => {
+      const response = await fetch(`${this.url}/version`, {
         mode: "cors",
         headers: { "Content-Type": "application/json" },
       });
