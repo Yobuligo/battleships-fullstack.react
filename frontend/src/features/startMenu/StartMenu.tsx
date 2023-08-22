fdfaa719482258ba29eec0243c5f35c081cf120c
Peter <hoffmannpeter82@gmail.com>
2023-08-22 14:06:15 +0200

    Translate texts for StartMenu

--- frontend/src/features/startMenu/StartMenu.tsx
+++ frontend/src/features/startMenu/StartMenu.tsx
@@ -1,12 +1,16 @@
 import { Button } from "../../components/button/Button";
+import { useTranslation } from "../../hooks/useTranslation";
 import styles from "./StartMenu.module.css";
 
 export const StartMenu: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section className={styles.startMenu}>
-      <Button>Create Game</Button>
-      <Button>Join Game</Button>
-      <Button>Setup</Button>
+      <Button>{t.startMenu.login}</Button>
+      <Button>{t.startMenu.createGame}</Button>
+      <Button>{t.startMenu.joinGame}</Button>
+      <Button>{t.startMenu.setup}</Button>
     </section>
   );
 };
