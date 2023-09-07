1c4daa8815d158f664db6d52640584b5d21d90a6
Yobuligo <hoffmannpeter82@gmail.com>
2023-09-07 21:12:31 +0200

    Add Todo to usePolling

--- frontend/src/hooks/usePolling.ts
+++ frontend/src/hooks/usePolling.ts
@@ -1,12 +1,14 @@
 import { useCallback } from "react";
 import { IPollRepository } from "../api/types/IPollRepository";
 import { IEntity } from "../shared/types/IEntity";
+import { Todo } from "../utils/Todo";
 
 export const usePolling = <T extends IEntity>(
   repository: IPollRepository<T>,
   reload: () => void,
   interval?: number
 ) => {
+  Todo("Refactor, is no custom hook");
   const onPoll = useCallback(() => {
     setTimeout(async () => {
       try {
